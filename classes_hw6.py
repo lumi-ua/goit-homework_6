7c9df1059df45ae91c85e0904d078acbb458ef5d
lumi-ua <130182058+lumi-ua@users.noreply.github.com>
2023-07-22 00:17:58 +0300

    Update classes_hw6.py

--- classes_hw6.py
+++ classes_hw6.py
@@ -5,20 +5,20 @@ import itertools
 
 class Field:
 
-    def __init__(self, var) -> None:
-        self._value = None
-        self.value = var
+    def __init__(self, value) -> None:
+        self.__value = None
+        self.value = value
 
     @property
     def value(self):
-        return self._value
+        return self.__value
 
     @value.setter
     def value(self, var):
-        self._value = var
+        self.__value = value
     
     def __str__(self) -> str:
-        return self._value
+        return self.__value
     
     def __repr__(self) -> str:
         return str(self)
@@ -32,15 +32,16 @@ class Phone(Field):
 
     @property
     def value(self):
-        return super().value
+        return self.__value
 
     @value.setter
-    def value(self, var):
-        if (len(var) == 12) and var.isnumeric():
+    def value(self, value):
+        if (len(value) == 12) and value.isnumeric():
             #Field.value = var
-            super(Phone, self.__class__).value.fset(self, var)
+            self.__value = value
+            # super(Phone, self.__class__).value.fset(self, var)
         else:
-            raise ValueError(f"{var} is an invalid mobile number")
+            raise ValueError(f"{value} is an invalid mobile number")
 
 
 class Birthday(Field):
@@ -48,23 +49,25 @@ class Birthday(Field):
     @property
     def value(self):
         #return Field.value
-        return super().value
+        return self.__value
 
     @value.setter
-    def value(self, var):
-        dtv = datetime.strptime(var, "%d.%m.%Y")
+    def value(self, value):
+        dtv = datetime.strptime(value, "%d.%m.%Y")
         current_year = datetime.now().year
         if dtv.year > current_year or dtv.year < current_year - 120:
             raise ValueError("Invalid birthday range!")
         else:
             #Field.value = dtv
-            super(Birthday, self.__class__).value.fset(self, dtv)
+            self.__value = dyv
+            # super(Birthday, self.__class__).value.fset(self, dtv)
 
         if dtv > datetime.now():
             raise ValueError("Invalid birthday!")
         else:
             #Field.value = dtv
-            super(Birthday, self.__class__).value.fset(self, dtv)
+            self.__value = dtv
+            # super(Birthday, self.__class__).value.fset(self, dtv)
 
 
 class Record:
